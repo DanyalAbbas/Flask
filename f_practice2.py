ceb56febdea09d8aacc90af5da5c0c35d7298ce6
Danyal Abbas <abasdaniel61@gmail.com>
2024-08-12 23:25:07 +0500

    html and stuff

---
+++ f_practice2.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=name, r=2, hell = ["chigga", "Danyal", "Zainab"])
+
+# @app.route("/admin")
+# def admin():
+#     return redirect(url_for("user", name="Admin!"))
+
+
+
+
+if __name__ == "__main__":
+    app.run()
