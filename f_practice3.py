884bf9a1f6ebdc469cc6daa73f27c4fced34f3df
Danyal Abbas <abasdaniel61@gmail.com>
2024-08-16 20:26:35 +0500

    heh

---
+++ f_practice3.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, url_for, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index1.html", content="Testing")
+
+@app.route("/test")
+def test():
+    return render_template("index.html", content="Testing")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
